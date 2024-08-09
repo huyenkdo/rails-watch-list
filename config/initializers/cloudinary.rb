783c54a141a74f4855d4b494118ec8864c8dce21
Huyen Do <huyenkdo@gmail.com>
2024-08-09 18:25:18 +0200

    Set up Cloudinary configuration for Heroku

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,7 @@
+Cloudinary.config do |config|
+  config.cloud_name = ENV['did0eciid']
+  config.api_key = ENV['511948147528143']
+  config.api_secret = ENV['btU6tCETfCYyguQD7y6mDcqHAS4']
+  config.secure = true
+  config.cdn_subdomain = true
+end
